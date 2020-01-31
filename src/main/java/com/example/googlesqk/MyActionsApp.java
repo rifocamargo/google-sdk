47fc0d30b6ca511b28a4a4bd7e19d391b7ff096f
Ricardo Camargo <rifocamargo@gmail.com>
2020-01-31 14:51:34 -0300

    change intent

--- src/main/java/com/example/googlesqk/MyActionsApp.java
+++ src/main/java/com/example/googlesqk/MyActionsApp.java
@@ -17,7 +17,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 public class MyActionsApp extends DialogflowApp {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MyActionsApp.class);
-	
+
 	@Autowired
 	private TokenDecoder tokenDecoder;
 
@@ -33,13 +33,12 @@ public class MyActionsApp extends DialogflowApp {
 		if (this.userIsSignedIn(request)) {
 			LOGGER.info("Retrieving user from token '{}'", request.getUser().getIdToken());
 			GoogleIdToken.Payload profile = getUserProfile(request.getUser().getIdToken());
-			responseBuilder
-					.add("Olá " + profile.get("given_name") + "!. Vamos começar?");
+			responseBuilder.add("Olá ").add(profile.get("given_name").toString()).add("! Vamos começar?");
 		} else {
 			responseBuilder.add("Usuário não está logado");
 		}
 		return responseBuilder.build();
-		
+
 	}
 
 	@ForIntent("signin")
@@ -52,15 +51,14 @@ public class MyActionsApp extends DialogflowApp {
 		try {
 			profile = tokenDecoder.decodeIdToken(idToken);
 		} catch (Exception e) {
-			LOGGER.error("error decoding idtoken");
-			LOGGER.error(e.toString());
+			LOGGER.error("error decoding idtoken", e);
 		}
 		return profile;
 	}
-	
+
 	private boolean userIsSignedIn(ActionRequest request) {
 		String idToken = request.getUser().getIdToken();
-		LOGGER.info(String.format("Id token: %s", idToken));
+		LOGGER.info("Id token: {}", idToken);
 		if (idToken == null || idToken.isEmpty()) {
 			return false;
 		} else {
@@ -68,106 +66,3 @@ public class MyActionsApp extends DialogflowApp {
 		}
 	}
 }
-
-//import com.google.actions.api.ActionRequest;
-//import com.google.actions.api.ActionResponse;
-//import com.google.actions.api.DialogflowApp;
-//import com.google.actions.api.ForIntent;
-//import com.google.actions.api.response.ResponseBuilder;
-//import com.google.actions.api.response.helperintent.SignIn;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * Tip: Sign In should not happen in the Default Welcome Intent, instead later
-// * in the conversation. See `Action discovery` docs:
-// * https://developers.google.com/actions/discovery/implicit#action_discovery
-// */
-//@Service
-//public class MyActionsApp extends DialogflowApp {
-//
-//	private static final Logger LOGGER = LoggerFactory.getLogger(MyActionsApp.class);
-//
-//	@Autowired
-//	private TokenDecoder tokenDecoder;
-//
-//	@ForIntent("Default Welcome Intent")
-//	public ActionResponse welcome(ActionRequest request) {
-//		LOGGER.info("Welcome intent start.");
-//		ResponseBuilder responseBuilder = getResponseBuilder(request);
-//
-//		// Account Linking is only supported for verified users
-//		// https://developers.google.com/actions/assistant/guest-users
-//		if (!request.getUser().getUserVerificationStatus().equals("VERIFIED")) {
-//			responseBuilder.add("Olá Visitante");
-//			responseBuilder.endConversation();
-//			return responseBuilder.build();
-//		}
-//
-//		if (userIsSignedIn(request)) {
-//			GoogleIdToken.Payload profile = getUserProfile(request.getUser().getIdToken());
-//			responseBuilder.add(String.format("Hi %s!", profile.get("given_name")));
-//
-//		} else {
-//			responseBuilder.add(String.format("Hi", ""));
-//			responseBuilder.add("What is your favorite color?");
-//		}
-//		responseBuilder.addSuggestions(new String[] { "Red", "Green", "Blue" });
-//		LOGGER.info("Welcome intent end.");
-//		return responseBuilder.build();
-//	}
-//
-//	@ForIntent("Give Color")
-//	public ActionResponse giveColor(ActionRequest request) {
-//		LOGGER.info("Give color intent start.");
-//		ResponseBuilder responseBuilder = getResponseBuilder(request);
-//		if (userIsSignedIn(request)) {
-//			GoogleIdToken.Payload profile = getUserProfile(request.getUser().getIdToken());
-//
-//			responseBuilder.add("Since you are signed in, I'll remember it next time.").add(profile.getEmail()).endConversation();
-//			LOGGER.info("Give color intent end.");
-//			return responseBuilder.build();
-//		}
-//		responseBuilder.add(new SignIn());
-//		LOGGER.info("Give color intent end.");
-//		return responseBuilder.build();
-//	}
-//
-//	@ForIntent("Get Sign In")
-//	public ActionResponse getSignIn(ActionRequest request) {
-//		LOGGER.info("Get sign in intent start.");
-//		ResponseBuilder responseBuilder = getResponseBuilder(request);
-//		if (request.isSignInGranted()) {
-//			GoogleIdToken.Payload profile = getUserProfile(request.getUser().getIdToken());
-//			responseBuilder.add("Since you are signed in, I'll remember it next time.").add(profile.getEmail()).endConversation();
-//		} else {
-//			responseBuilder.add("Let's try again next time.").endConversation();
-//		}
-//		LOGGER.info("Get sign in intent end.");
-//		return responseBuilder.build();
-//	}
-//
-//	private boolean userIsSignedIn(ActionRequest request) {
-//		String idToken = request.getUser().getIdToken();
-//		LOGGER.info(String.format("Id token: %s", idToken));
-//		if (idToken == null || idToken.isEmpty()) {
-//			return false;
-//		} else {
-//			return true;
-//		}
-//	}
-//
-//	private GoogleIdToken.Payload getUserProfile(String idToken) {
-//		LOGGER.info("Retrieving user from token '{}'", idToken);
-//		GoogleIdToken.Payload profile = null;
-//		try {
-//			profile = tokenDecoder.decodeIdToken(idToken);
-//		} catch (Exception e) {
-//			LOGGER.error("error decoding idtoken", e);
-//		}
-//		return profile;
-//	}
-//}
--- src/main/java/com/example/googlesqk/TokenDecoder.java
+++ src/main/java/com/example/googlesqk/TokenDecoder.java
@@ -22,83 +22,9 @@ public class TokenDecoder {
 		JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
 		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-				// Specify the CLIENT_ID of the app that accesses the backend:
 				.setAudience(Collections.singletonList(CLIENT_ID)).build();
 
 		GoogleIdToken idToken = verifier.verify(idTokenString);
 		return idToken.getPayload();
 	}
-
-	// public GoogleIdToken.Payload decodeIdToken(String idTokenString) throws
-	// GeneralSecurityException, IOException {
-	// HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
-	// JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-	//
-	// GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport,
-	// jsonFactory)
-	// // Specify the CLIENT_ID of the app that accesses the backend:
-	// .setAudience(Collections.singletonList(CLIENT_ID)).build();
-	//
-	// GoogleIdToken idToken = GoogleIdToken.parse(verifier.getJsonFactory(),
-	// idTokenString);
-	//
-	// if (this.verify(verifier, idToken)) {
-	// LOGGER.info("Token is valid");
-	// return idToken.getPayload();
-	// } else {
-	// LOGGER.info("Token is invalid");
-	// return null;
-	// }
-	//
-	// }
-	//
-	// public boolean verify(GoogleIdTokenVerifier verifier, GoogleIdToken
-	// googleIdToken)
-	// throws GeneralSecurityException, IOException {
-	// if (!this.isPaylodOk(verifier, googleIdToken)) {
-	// LOGGER.info("Paylod is not ok");
-	// LOGGER.info("googleIdToken.verifyIssuer(verifier.getIssuers()) = {}",
-	// googleIdToken.verifyIssuer(verifier.getIssuers()));
-	//
-	//// LOGGER.info("googleIdToken.verifyAudience(verifier.getAudience()) = {}",
-	//// googleIdToken.verifyAudience(verifier.getAudience()));
-	////
-	//// LOGGER.info("googleIdToken.getPayload().getAudienceAsList().isEmpty() =
-	// {}",
-	//// googleIdToken.getPayload().getAudienceAsList().isEmpty());
-	//// googleIdToken.getPayload().getAudienceAsList().stream()
-	//// .forEach(aud ->
-	// LOGGER.info("googleIdToken.getPayload().getAudienceAsList().stream() = {}",
-	// aud));
-	////
-	//// LOGGER.info("verifier.getAudience().containsAll(googleIdToken.getPayload().getAudienceAsList())
-	// = {}",
-	//// verifier.getAudience().containsAll(googleIdToken.getPayload().getAudienceAsList()));
-	//// verifier.getAudience().stream().forEach(aud ->
-	// LOGGER.info("verifier.getAudience().stream() = {}", aud));
-	//
-	// LOGGER.info("googleIdToken.verifyTime(Clock.SYSTEM.currentTimeMillis(), 300)
-	// = {}",
-	// googleIdToken.verifyTime(Clock.SYSTEM.currentTimeMillis(), 300));
-	// return false;
-	// }
-	// for (PublicKey publicKey : verifier.getPublicKeys()) {
-	// LOGGER.info("Verifying public kay signature. {}", publicKey.toString());
-	// if (googleIdToken.verifySignature(publicKey)) {
-	// LOGGER.info("Signature is valid");
-	// return true;
-	// }
-	// }
-	//
-	// return false;
-	// }
-	//
-	// private boolean isPaylodOk(GoogleIdTokenVerifier verifier, GoogleIdToken
-	// googleIdToken) {
-	//// return googleIdToken.verifyIssuer(verifier.getIssuers()) &&
-	// googleIdToken.verifyAudience(verifier.getAudience())
-	//// && googleIdToken.verifyTime(Clock.SYSTEM.currentTimeMillis(), 300);
-	// return googleIdToken.verifyIssuer(verifier.getIssuers())
-	// && googleIdToken.verifyTime(Clock.SYSTEM.currentTimeMillis(), 300);
-	// }
 }
